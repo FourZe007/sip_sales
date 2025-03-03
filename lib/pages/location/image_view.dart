b654b74b076f8a62f323b573960e01a5448a53f7
FourZe007 <josephangelus.jobs@gmail.com>
2025-03-03 10:52:36 +0700

    iOS Patch v1.1.9, 03-02-2025 (Change coordinate text to maps button in manager act details)

--- lib/pages/location/image_view.dart
+++ lib/pages/location/image_view.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:provider/provider.dart';
+import 'package:sip_sales/global/dialog.dart';
 import 'package:sip_sales/global/global.dart';
+import 'package:sip_sales/global/state_management.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
-class ImageView extends StatelessWidget {
+class ImageView extends StatefulWidget {
   ImageView(
     this.imageDir, {
     this.lat = 0.0,
@@ -25,7 +29,38 @@ class ImageView extends StatelessWidget {
   bool isManager;
 
   @override
+  State<ImageView> createState() => _ImageViewState();
+}
+
+class _ImageViewState extends State<ImageView> {
+  void openMap(
+    SipSalesState state,
+    double lat,
+    double lng,
+  ) async {
+    Uri url = Uri.parse('https://maps.google.com/?q=$lat,$lng');
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      if (mounted) {
+        // Custom Alert Dialog for Android and iOS
+        GlobalDialog.showCrossPlatformDialog(
+          context,
+          'Peringatan!',
+          'Tidak dapat membuka tautan. Silakan periksa URL dan coba lagi.',
+          () => Navigator.pop(context),
+          'Tutup',
+          isIOS: (Platform.isIOS) ? true : false,
+        );
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final state = Provider.of<SipSalesState>(context);
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: MediaQuery.of(context).size.height * 0.025,
@@ -66,75 +101,103 @@ class ImageView extends StatelessWidget {
                           ClipRRect(
                             borderRadius: BorderRadius.circular(20.0),
                             child: Image.memory(
-                              base64Decode(imageDir),
+                              base64Decode(widget.imageDir),
                               fit: BoxFit.cover,
                               scale: 0.55,
                               height: MediaQuery.of(context).size.height * 0.5,
                             ),
                           ),
-                          Builder(
-                            builder: (context) {
-                              if (isManager) {
-                                if (startTime != '') {
-                                  return Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey[350],
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    padding: const EdgeInsets.all(5.0),
-                                    margin: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      startTime,
-                                      style: GlobalFont.mediumgiantfontRBold,
-                                    ),
-                                  );
-                                } else {
-                                  return const SizedBox();
-                                }
-                              } else {
-                                if (startTime != '' && endTime != '') {
-                                  return Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey[350],
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    padding: const EdgeInsets.all(5.0),
-                                    margin: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      '$startTime - $endTime',
-                                      style: GlobalFont.mediumgiantfontRBold,
-                                    ),
-                                  );
-                                } else {
-                                  return const SizedBox();
-                                }
-                              }
-                            },
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.015,
+                              vertical:
+                                  MediaQuery.of(context).size.height * 0.0075,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Builder(
+                                  builder: (context) {
+                                    if (widget.isManager) {
+                                      if (widget.startTime != '') {
+                                        return Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey[350],
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                          padding: const EdgeInsets.all(5.0),
+                                          margin: const EdgeInsets.all(5.0),
+                                          child: Text(
+                                            widget.startTime,
+                                            style:
+                                                GlobalFont.mediumgiantfontRBold,
+                                          ),
+                                        );
+                                      } else {
+                                        return const SizedBox();
+                                      }
+                                    } else {
+                                      if (widget.startTime != '' &&
+                                          widget.endTime != '') {
+                                        return Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey[350],
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                          ),
+                                          padding: const EdgeInsets.all(5.0),
+                                          margin: const EdgeInsets.all(5.0),
+                                          child: Text(
+                                            '${widget.startTime} - ${widget.endTime}',
+                                            style:
+                                                GlobalFont.mediumgiantfontRBold,
+                                          ),
+                                        );
+                                      } else {
+                                        return const SizedBox();
+                                      }
+                                    }
+                                  },
+                                ),
+                                Builder(
+                                  builder: (context) {
+                                    if (widget.lat != 0.0 &&
+                                        widget.lng != 0.0) {
+                                      return GestureDetector(
+                                        onTap: () => openMap(
+                                          state,
+                                          widget.lat,
+                                          widget.lng,
+                                        ),
+                                        child: CircleAvatar(
+                                          radius: 25,
+                                          backgroundColor: Colors.grey[350],
+                                          child: Padding(
+                                            padding: EdgeInsets.symmetric(
+                                              horizontal: 5,
+                                              vertical: 5,
+                                            ),
+                                            child: Icon(
+                                              Icons.map_rounded,
+                                              size: 30,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    } else {
+                                      return const SizedBox();
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    Builder(
-                      builder: (context) {
-                        if (lat != 0.0 && lng != 0.0) {
-                          return Container(
-                            height: MediaQuery.of(context).size.height * 0.045,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.grey[400],
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            padding: const EdgeInsets.all(5.0),
-                            margin: const EdgeInsets.all(5.0),
-                            child: Text(
-                              '($lat, $lng)',
-                              style: GlobalFont.mediumgiantfontRBold,
-                            ),
-                          );
-                        } else {
-                          return const SizedBox();
-                        }
-                      },
                     ),
                   ],
                 ),
